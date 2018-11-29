83287877073ee897d62b1f48360a2d920955a8a9
unknown <chance.farr97@gmail.com>
2018-11-29 13:55:48 -0700

    prod debug

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ const socket = require('socket.io')
 const creationController = require('./creationController');
 require('dotenv').config();
 const app = express();
-app.use(express.json())
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
 
 app.use( express.static( `${__dirname}/../build` ) );
 
-const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
+app.use(express.json())
 app.use(session({
     secret: SESSION_SECRET,
     resave:false,
@@ -22,12 +22,12 @@ massive(CONNECTION_STRING).then(db => {
     console.log('database connected')
 })
 //Login Endpoints
-app.post('/auth/login', authController.loginPlayer)
-app.post('/auth/register',authController.newPlayer)
-app.get('/auth/logout', authController.logout)
-app.get('/auth/check', authController.checkLogin)
-//Other Endpoints
-app.get('/api/races', creationController.getRaces)
+// app.post('/auth/login', authController.loginPlayer)
+// app.post('/auth/register',authController.newPlayer)
+// app.get('/auth/logout', authController.logout)
+// app.get('/auth/check', authController.checkLogin)
+// //Other Endpoints
+// app.get('/api/races', creationController.getRaces)
 //Server Generic
 app.listen(SERVER_PORT, () =>{
     console.log(`hatching wyverian eggs on port ${SERVER_PORT}`)
